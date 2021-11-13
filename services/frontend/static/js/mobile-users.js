af5bc3ee5fc013fbf69dc35690d2ed4b18705e73
Philipp Stuerner <philipp.stuerner@web.de>
2021-11-13 10:55:00 +0100

    add mobile user alert as js script

---
+++ services/frontend/static/js/mobile-users.js
@@ -0,0 +1,3 @@
+if (typeof screen.orientation !== 'undefined') {} else {
+    alert('Hi mobile user 👋\nThis post is not optimized for smartphones. Consider using a desktop device to get the best chart and animation experience. Apologies!')
+};
--- services/frontend/static/js/posts/confusion_matrix/cm.js
+++ services/frontend/static/js/posts/confusion_matrix/cm.js
@@ -1,9 +1,5 @@
 import {responsivefy} from '../../helpers.js';
 
-if (typeof screen.orientation !== 'undefined') {} else {
-    alert('Hi mobile user 👋\nThis post is not optimized for smartphones. Consider using a desktop device to get the best chart and animation experience. Apologies!')
-};
-
 d3.csv('/static/data/rf_pred.csv').then(function(dataRaw) {
     function computeConfusionMatrix (cutoff) {
         cm = {
