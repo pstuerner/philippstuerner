3d82aeb98de533a78cf89dd948878dc6b08b3222
Philipp Stuerner <philipp.stuerner@web.de>
2023-06-05 10:52:35 +0200

    fix dockerfile build (use buildx), add d3-3d to dependencies, fix totopbutton

--- services/frontend/src/components/ToTopButton.vue
+++ services/frontend/src/components/ToTopButton.vue
@@ -1,16 +1,43 @@
 <template>
-    <button onclick="topFunction()" id="to-top-button" title="Go to top"><i class="fas fa-arrow-up"></i></button>
+    <button @click="topFunction" id="to-top-button" title="Go to top"><i class="fas fa-arrow-up"></i></button>
 </template>
 
 <script>
 export default {
+    data () {
+        return {
+            mybutton: null
+        }
+    },
     mounted() {
-        let src = "/js/back-to-top.js";
-        let script = document.createElement("script");
-        script.setAttribute("src", src);
-        script.setAttribute("id", src)
-        script.async = false;
-        document.body.appendChild(script);
+        var vm = this;
+
+        //Get the button:
+        vm.mybutton = document.getElementById("to-top-button");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function () {
+            vm.scrollFunction();
+        };
+    },
+    methods: {
+        scrollFunction () {
+            var vm = this;
+            
+            if (
+                document.body.scrollTop > 20 ||
+                document.documentElement.scrollTop > 20
+            ) {
+                vm.mybutton.style.display = "block";
+            } else {
+                vm.mybutton.style.display = "none";
+            }
+        },
+        // When the user clicks on the button, scroll to the top of the document
+        topFunction () {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
     }
 }
 </script>
